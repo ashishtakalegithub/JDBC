fb4d8e83930e438e41a4da2c8e5deb82d138490d
Ashish <ashishtakale@gmail.com>
2023-01-23 11:46:26 +0530

    driver register explicitely

--- src/com/practice/jdbc/JdbcDemo2.java
+++ src/com/practice/jdbc/JdbcDemo2.java
@@ -12,7 +12,7 @@ public class JdbcDemo2 {
 		String pwd = "Ashish@09";
 		String sqlQuery = "insert into books(title,author_fname,author_lname,released_year,stock_quantity,pages) values('The Namesake','Jhumpa','Lahiri',2003,250,100),('Norse Mythology','Neil','Gaiman',2016,250,150),('American Gods','Neil','Gaiman',2001,200,250)";
 
-		try (Connection con = DriverManager.getConnection(url, user, pwd); Statement st = con.createStatement()) {
+		try (Connection con = DriverManager.getConnection(url , user, pwd); Statement st = con.createStatement()) {
 			Class.forName("com.mysql.jdbc.Driver");
 			st.executeUpdate(sqlQuery);
 
